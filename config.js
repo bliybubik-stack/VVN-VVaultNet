0363fa4788c340d6c2ec0a29fa572a66535a8d34
bliybubik-stack <bliybubik@gmail.com>
2026-07-12 00:51:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,4 +26,9 @@ const CONFIG = {
     }
 };
 
+// Make CONFIG globally available
 window.CONFIG = CONFIG;
+
+console.log('🔐 VVN Config loaded');
+console.log('👤 Owners:', CONFIG.OWNERS);
+console.log('🔑 Developer PIN set');
